ecbba7fc7a4290f224a2050f167de1d3730acc5c
sesiria <stjohnson_free@hotmail.com>
2018-04-12 10:59:18 +0800

    add implementation and test case of binomialQueue

--- Lib/BinomialQueue.h
+++ Lib/BinomialQueue.h
@@ -8,25 +8,208 @@ template <typename Comparable>
 class BinomialQueue
 {
   public:
-    BinomialQueue();
-    BinomialQueue(const Comparable &item);
-    BinomialQueue(BinomialQueue &&rhs);
+    BinomialQueue() : theTrees(DEFAULT_TREES)
+    {
+        for (auto &root : theTrees)
+            root = nullptr;
+        currentSize = 0;
+    }
 
-    ~BinomialQueue();
+    BinomialQueue(const Comparable &item)
+        : theTrees(1), currentSize(1)
+    {
+        theTrees[0] = new BinomialNode{item, nullptr, nullptr};
+    }
 
-    BinomialQueue &operator=(const BinomialQueue &rhs);
-    BinomialQueue &operator=(BinomialQueue &&rhs);
+    BinomialQueue(const BinomialQueue &rhs)
+        : theTrees(rhs.theTrees.size()), currentSize(rhs.currentSize)
+    {
+        for (int i = 0; i < rhs.theTrees.size(); ++i)
+            theTrees[i] = clone(rhs.theTrees[i]);
+    }
 
-    bool isEmpty() const;
-    const Comparable &findMin() const;
+    BinomialQueue(BinomialQueue &&rhs)
+        : theTrees(std::move(rhs.theTrees)), currentSize(rhs.currentSize)
+    {
+        rhs.currentSize = 0;
+    }
 
-    void insert(const Comparable &x);
-    void insert(Comparable &&x);
-    void deleteMin();
-    void deleteMin(Comparable &minItem);
+    ~BinomialQueue()
+    {
+        makeEmpty();
+    }
 
-    void makeEmpty();
-    void merge(BinomialQueue &rhs);
+    /**
+     * Deep copy
+     */
+    BinomialQueue &operator=(const BinomialQueue &rhs)
+    {
+        BinomialQueue copy = rhs;
+        std::swap(*this, copy);
+        return *this;
+    }
+
+    /**
+     * Move
+     */
+    BinomialQueue &operator=(BinomialQueue &&rhs)
+    {
+        std::swap(currentSize, rhs.currentSize);
+        std::swap(theTrees, rhs.theTrees);
+        return *this;
+    }
+
+    bool isEmpty() const
+    {
+        return currentSize == 0;
+    }
+
+    const Comparable &findMin() const
+    {
+        if (isEmpty())
+            std::__throw_underflow_error(
+                "Try to delete element from an empty heap");
+
+        return theTrees[findMinIndex()]->element;
+    }
+
+    /**
+     * Insert item x into the priority queue; allows duplicates.
+     */
+    void insert(const Comparable &x)
+    {
+        BinomialQueue temp{x};
+        merge(temp);
+    }
+
+    /**
+     * Insert item x into the priority queue; allows duplicates.
+     */
+    void insert(Comparable &&x)
+    {
+        merge(BinomialQueue{std::move(x)});
+    }
+
+    /**
+     * Remove the smallest item from the priority queue.
+     * Throws UnderflowException if empty.
+     */
+    void deleteMin()
+    {
+        Comparable x;
+        deleteMin(x);
+    }
+
+    /**
+     * Remove the minimum item and place it in minItem.
+     */
+    void deleteMin(Comparable &minItem)
+    {
+        if (isEmpty())
+            std::__throw_underflow_error(
+                "Try to delete element from an empty heap");
+
+        int minIndex = findMinIndex();
+        minItem = theTrees[minIndex]->element;
+
+        BinomialNode *oldRoot = theTrees[minIndex];
+        BinomialNode *deletedTree = oldRoot->leftChild;
+        delete oldRoot;
+
+        // Construct H''
+        BinomialQueue deletedQueue;
+        deletedQueue.theTrees.resize(minIndex);
+        deletedQueue.currentSize = (1 << minIndex) - 1;
+        for (int j = minIndex - 1; j >= 0; --j)
+        {
+            deletedQueue.theTrees[j] = deletedTree;
+            deletedTree = deletedTree->nextSibling;
+            deletedQueue.theTrees[j]->nextSibling = nullptr;
+        }
+
+        // Construct H'
+        theTrees[minIndex] = nullptr;
+        currentSize -= deletedQueue.currentSize + 1;
+
+        merge(deletedQueue);
+    }
+
+    /**
+     * Make the priority queue logically empty.
+     */
+    void makeEmpty()
+    {
+        currentSize = 0;
+        for(auto & root : theTrees)
+            makeEmpty(root);
+    }
+
+    /**
+     * Merge rhs into the priority queue.
+     * rhs becomes empty. rhs must be different from this.
+     */
+    void merge(BinomialQueue &rhs)
+    {
+        if (this == &rhs) // Avoid aliasing problems
+            return;
+
+        currentSize += rhs.currentSize;
+
+        if (currentSize > capacity())
+        {
+            int oldNumTrees = theTrees.size();
+            int newNumTrees = std::max(theTrees.size(), rhs.theTrees.size()) + 1;
+            theTrees.resize(newNumTrees);
+            for (int i = oldNumTrees; i < newNumTrees; ++i)
+                theTrees[i] = nullptr;
+        }
+
+        BinomialNode *carry = nullptr;
+        for (int i = 0, j = 1; j <= currentSize; ++i, j *= 2)
+        {
+            BinomialNode *t1 = theTrees[i];
+            BinomialNode *t2 = i < rhs.theTrees.size() ? rhs.theTrees[i] : nullptr;
+            int whichCase = t1 == nullptr ? 0 : 1;
+            whichCase += t2 == nullptr ? 0 : 2;
+            whichCase += carry == nullptr ? 0 : 4;
+
+            switch (whichCase)
+            {
+            case 0: /* No trees */
+            case 1: /* Only this */
+                break;
+            case 2: /* only rhs */
+                theTrees[i] = t2;
+                rhs.theTrees[i] = nullptr;
+                break;
+            case 4: /* only carry */
+                theTrees[i] = carry;
+                carry = nullptr;
+                break;
+            case 3: /* this and rhs */
+                carry = combineTrees(t1, t2);
+                theTrees[i] = rhs.theTrees[i] = nullptr;
+                break;
+            case 5: /* this and carry */
+                carry = combineTrees(t1, carry);
+                theTrees[i] = nullptr;
+                break;
+            case 6: /* rhs and carry */
+                carry = combineTrees(t2, carry);
+                rhs.theTrees[i] = nullptr;
+                break;
+            case 7: /* All three */
+                theTrees[i] = carry;
+                carry = combineTrees(t1, t2);
+                rhs.theTrees[i] = nullptr;
+                break;
+            }
+        }
+
+        for (auto &root : rhs.theTrees)
+            root = nullptr;
+        rhs.currentSize = 0;
+    }
 
   private:
     struct BinomialNode
@@ -47,11 +230,70 @@ class BinomialQueue
     std::vector<BinomialNode *> theTrees; // An array of tree roots
     int currentSize;                      // Number of items in the priority queue
 
-    int findMinIndex() const;
-    int capacity() const;
-    BinomialNode *combineTrees(BinomialNode *t1, BinomialNode *t2);
-    void makeEmpty(BinomialNode *&t);
-    BinomialNode *clone(BinomialNode *t) const;
+    /**
+     * Find index of tree containing the smallest item in the priority queue.
+     * The priority queue must not be empty.
+     * Return the index of tree containing the smallest item.
+     */
+    int findMinIndex() const
+    {
+        int i;
+        int minIndex;
+
+        for (i = 0; theTrees[i] == nullptr; ++i)
+            ;
+
+        for (minIndex = i; i < theTrees.size(); ++i)
+            if (theTrees[i] != nullptr &&
+                theTrees[i]->element < theTrees[minIndex]->element)
+                minIndex = i;
+        return minIndex;
+    }
+
+    /**
+     * Return the capacity.
+     */
+    int capacity() const
+    {
+        return (1 << theTrees.size()) - 1; // the size is 2^K - 1
+    }
+
+    /**
+     * Return the result of merging equal-sized t1 and t2.
+     */
+    BinomialNode *combineTrees(BinomialNode *t1, BinomialNode *t2)
+    {
+        if (t2->element < t1->element)
+            return combineTrees(t2, t1);
+        t2->nextSibling = t1->leftChild;
+        t1->leftChild = t2;
+        return t1;
+    }
+
+    /**
+     * Make a binomial tree logically empty, and free memory.
+     */
+    void makeEmpty(BinomialNode *&t)
+    {
+        if(t != nullptr)
+        {
+            makeEmpty(t->leftChild);
+            makeEmpty(t->nextSibling);
+            delete t;
+            t = nullptr;
+        }
+    }
+
+    /**
+     * Internal method to clone subtree.
+     */
+    BinomialNode *clone(BinomialNode *t) const
+    {
+        if(t == nullptr)
+            return nullptr;
+        else
+            return new BinomialNode{t->element, clone(t->leftChild), clone(t->nextSibling)};
+    }
 };
 
 #endif
